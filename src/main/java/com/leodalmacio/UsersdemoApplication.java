8e1ffa41b75de5289e43dfe9d42f69dc595b370b
Leonard Dalmacio <leonarddalmacio@Thrush.local>
2018-04-30 10:20:21 +0800

    Completed Users API

---
+++ src/main/java/com/leodalmacio/UsersdemoApplication.java
@@ -0,0 +1,12 @@
+package com.leodalmacio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersdemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/leodalmacio/UsersdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.leodalmacio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UsersdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
